7df12b68f29fc4346a2007a77b9bdcdf13312507
TemplarManatee <dylanbrad10@gmail.com>
2023-03-01 16:52:20 -0500

    Tag Routes completed

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,22 +3,67 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll();
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      // JOIN with locations, using the Trip through table
+      include: [{ model: Product, through: ProductTag, as: 'product_id' }]
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(
+      {
+        tag_name: req.body.tag_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      });
+    if (!tagData) {
+      res.status(404).json({ message: 'No Tag found with that ID.' });
+      return;
+    }
+    res.json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
@@ -31,10 +76,10 @@ router.delete('/:id', async (req, res) => {
         id: tagId
       }
     });
-    
-    if(!response) {
-      res.status(404).json({message: 'No tag found with that id.'});
-      return; 
+
+    if (!response) {
+      res.status(404).json({ message: 'No Tag found with that id.' });
+      return;
     }
 
     res.status(200).json(response);
